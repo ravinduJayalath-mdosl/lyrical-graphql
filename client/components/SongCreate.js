1ab6418de0d6e34e866a8b656145ad17aed2ad37
Ravindu Jayalath <ravindu@mydigitaloffice.ca>
2022-03-12 21:27:58 +0530

    feat(Song-create): Add mutations and params
    Added query mutations to create a song. Added params to mutations and pass arguments from song create component to mutation and completed the functionality to create a song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,5 +1,6 @@
-import { extendSchema } from 'graphql';
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongCreate extends Component {
     constructor(props) {
@@ -8,11 +9,19 @@ class SongCreate extends Component {
         this.state = { title: '' };
     }
 
+    onSubmit(event) {
+        this.props.mutate({
+            variables: {
+                title: this.state.title
+            }
+        });
+    }
+
     render() {
         return (
             <div>
                 <h3>Create a New Song</h3>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
                     <input onChange={event => this.setState({ title: event.target.value })}/>
                 </form>
@@ -21,4 +30,12 @@ class SongCreate extends Component {
     }
 }
 
-export default SongCreate;
+const mutation = gql`
+    mutation AddSong($title: String) {
+        addSong(title: $title) {
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
