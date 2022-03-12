b95440015c352ed3fff0454c32e1fb3423bc28ce
Ravindu Jayalath <ravindu@mydigitaloffice.ca>
2022-03-12 22:53:28 +0530

    feat(Song-delete): Add mutations to delete a song
    Added query mutations to delete a song. Included query and mutation in a single file. Seperate common queries to a query file.

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
     constructor(props) {
@@ -10,20 +12,24 @@ class SongCreate extends Component {
     }
 
     onSubmit(event) {
+        event.preventDefault();
+
         this.props.mutate({
             variables: {
                 title: this.state.title
-            }
-        });
+            },
+            refetchQueries: [{ query }]
+        }).then(() => { hashHistory.push('/') })
     }
 
     render() {
         return (
             <div>
+                <Link to="/">Back</Link>
                 <h3>Create a New Song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
-                    <input onChange={event => this.setState({ title: event.target.value })}/>
+                    <input onChange={event => this.setState({ title: event.target.value })} value={ this.state.title }/>
                 </form>
             </div>
         );
